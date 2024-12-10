50962aa0f51c8fdd2cf8984159d2178936bec4e9
Pedro Gabriel <pedrp@gmail.com>
2024-12-10 08:18:16 -0300

    Iniciando os estudos em c++

--- First Activity/first.cpp
+++ First Activity/first.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Vou aprender a programar na Linguagem C++" << endl;
-
+    cout << "\033[c1;c2 Vou \n aprender \n a\n programar\n na\n Linguagem\n C++ \033[0" << endl;
+    cout << "\033[31mTexto em vermelho\033[0m" << endl; // Vermelho
+    cout << "\033[32m" << "Texto em verde" << "\033[0m" << endl;   // Verde
+    cout << "\033[34m" << "Texto em azul" << "\033[0m" << endl;    // Azul
     return 0;
 }
