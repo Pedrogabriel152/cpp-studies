1153be3a3beb43b2546115b3d3622b7ddb7beb67
Pedro Gabriel <pedrp@gmail.com>
2024-12-16 11:18:56 -0300

    Arrays c++

---
+++ ArraysVetores/arraysVetores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int vet[10];
+    for (int i = 0; i < 10; i++) {
+        vet[i] = i;
+        cout << vet[i] << endl;
+    }
+    return 0;
+}
