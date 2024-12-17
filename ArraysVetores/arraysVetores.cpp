7a6c63ad166e3755740fd59274c36d155ce5f7b0
Pedro Gabriel <pedrp@gmail.com>
2024-12-17 10:06:40 -0300

    Arrays c++

--- ArraysVetores/arraysVetores.cpp
+++ ArraysVetores/arraysVetores.cpp
@@ -4,10 +4,24 @@
 using namespace std;
 
 int main() {
-    int vet[10];
-    for (int i = 0; i < 10; i++) {
-        vet[i] = i;
-        cout << vet[i] << endl;
+    float notes[5] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
+
+    float media, sum = 0.0f;
+    std::string nameStudent;
+
+    cout << "\nDigite o nome do Aluno: ";
+    cin >> nameStudent;
+
+    for (int i = 0; i < 5; i++) {
+        cout << "\nDigite a nota " << (i+1)<< ": ";
+        cin >> notes[i];
+        sum += notes[i];
+        system("clear");
     }
+
+    media = sum / 5;
+
+    cout << "\n*** Resultado das operacoes***\n";
+    cout << "\nA Media das notas do aluno foi: " << media << "\n";
     return 0;
 }
